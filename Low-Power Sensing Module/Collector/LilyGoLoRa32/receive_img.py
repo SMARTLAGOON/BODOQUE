684038ab0f1a0cd704efc5b1e33c131b6f135a47
Baarratia <baarratia@uc.cl>
2023-08-29 09:23:35 +0200

    Adding some try/excepts

--- Low-Power Sensing Module/Collector/LilyGoLoRa32/receive_img.py
+++ Low-Power Sensing Module/Collector/LilyGoLoRa32/receive_img.py
@@ -1,4 +1,6 @@
 import ujson
+from machine import Pin
+from time import sleep
 
 from lora32 import Lora32
 
@@ -9,30 +11,62 @@ from utils.uart_manager import UART_manager
 with open("Bodoque.json") as f:
     img_data = ujson.load(f)
 
-l = Lora32()
-screen = OLED_Screen(l, img_data, "Collector", "")
-sd = SD_manager()
-uart = UART_manager(br=9600)
+ # Set up green led as indicator of receiving data
+led = Pin(25, Pin.OUT)
+try:
+    l = Lora32()
+    screen = OLED_Screen(l, img_data, "Collector", "")
+except Exception as e:
+    print(e)
+    print("No screen")
+    screen = None
+    led.value(1)
+    sleep(1)
+    led.value(0)
+
+try:
+    sd = SD_manager()
+except Exception as e:
+    print(e)
+    print("No SD")
+    sd = None
+try:
+    uart = UART_manager(br=9600)
+except Exception as e:
+    print(e)
+    print("No UART")
+    uart = None
 
 def run():
-    screen.show_in_screen("Waiting...", "")
-    path = sd.get_path()
-    #Check the files in the SD for the biggest file name id_label.jpeg
-    files = sd.get_format_files(".jpeg")
-    print(files)
-    if files:
-        file_name_id = max([int(file.split("-")[0]) for file in files]) + 1
+    if screen:
+        screen.show_in_screen("Waiting...", "")
+    if sd:
+        path = sd.get_path()
+        #Check the files in the SD for the biggest file name id_label.jpeg
+        files = sd.get_format_files(".jpeg")
+        print(files)
+        if files:
+            file_name_id = max([int(file.split("-")[0]) for file in files]) + 1
+        else:
+            file_name_id = 0
+        while True:
+            try:
+                file_name = "{}".format(file_name_id)
+                label = uart.read_image(path, file_name)
+                if label:
+                    if screen:
+                        screen.show_in_screen("Received", label)
+                    print("Received", label)
+                    led.value(1)
+                    sleep(1)
+                    led.value(0)
+
+                    file_name_id += 1
+            except Exception as e:
+                print(e)
     else:
-        file_name_id = 0
-    while True:
-        try:
-            file_name = "{}".format(file_name_id)
-            label = uart.read_image(path, file_name)
-            if label:
-                screen.show_in_screen("Received", label)
-                file_name_id += 1
-        except Exception as e:
-            print(e)
+        if screen:
+            screen.show_in_screen("No SD", "")
 
 
 
